84d063d25306e58613a9ff23732f2e217759e928
madhuvenkat6 <madhuvenkatmedicharla@gmail.com>
2024-02-10 01:18:58 +0530

    inventory

---
+++ src/App.jsx
@@ -0,0 +1,370 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import DeleteIcon from '@mui/icons-material/Delete';
+import CreateIcon from '@mui/icons-material/Create';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
+import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
+import CategoryIcon from '@mui/icons-material/Category';
+
+import axios from 'axios';
+
+import Switch from '@mui/material/Switch';
+import BasicModal from './modal';
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
+
+function App() {
+  const [data, setData] = useState([{ "name": "Bluetooth", "category": "Electronic", "value": "black", "quantity": 5, "price": "$30" }, { "name": "Edifier M43560", "category": "Electronic", "value": "black", "quantity": 0, "price": "$0" }, { "name": "Sony 4k ultra 55 inch TV", "category": "Electronic", "value": "black", "quantity": 17, "price": "$70" }, { "name": "Samsumg 55 inch TV", "category": "Electronic", "value": "black", "quantity": 50, "price": "$12" }, { "name": "samsumg S34 Ultra", "category": "phone", "value": "black", "quantity": 0, "price": "$0" }])
+  const [checked, setChecked] = useState(false)
+  const [isloading, setIsloading] = useState(true)
+  const [openModal, setOpenModal] = useState(false)
+  const [editableProduct, setEditableProduct] = useState({})
+  const [disabledProducts, setDisabledProducts] = useState([])
+
+  const [overAllStoreDate, setOverAllStoreData] = useState({
+    total_products: 0,
+    total_value: 0,
+    out_of_stock: 0,
+    no_of_category: 0
+  })
+
+  const handleModalState = (val) => {
+    setOpenModal(val)
+  }
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
+  const handleDelete = (item, index, isEnabledProducts) => {
+
+    if (isEnabledProducts) {
+      let updatedProducts = data.filter((item, i) => i != index)
+      setData([...updatedProducts])
+    } else {
+      let updatedProducts = disabledProducts.filter((item, i) => i != index)
+      setDisabledProducts([...updatedProducts])
+    }
+
+
+    console.log(Number(item?.price?.substring(1) * item.quantity))
+
+
+  }
+
+  const handleDisable = (item, index) => {
+
+    let updatedProducts = data.filter((item, i) => i != index)
+    setData([...updatedProducts])
+    setDisabledProducts([...disabledProducts, item])
+
+  }
+
+  const handleEnable = (item, index) => {
+
+    let updatedProducts = disabledProducts.filter((item, i) => i != index)
+    setData([...data, item])
+    setDisabledProducts([...updatedProducts])
+
+  }
+
+  const handleEdit = (oldItem, newItem) => {
+
+    let updateData = data.map((item, index) => {
+      if (item.category == oldItem.category && item.price == oldItem.price && item.quantity == oldItem.quantity && item.value == oldItem.value) {
+        return newItem
+      } else {
+        return item;
+      }
+    })
+
+    setData([...updateData])
+    //edit the existing product data
+  }
+
+  const getData = async () => {
+    await axios.get('https://dev-0tf0hinghgjl39z.api.raw-labs.com/inventory')
+      .then((res) => {
+        setData(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const handleOverAllStoreData = () => {
+    let arr = [...data,...disabledProducts]
+
+    let category = {}
+    let total_amount = 0;
+
+    arr?.map((item) => {
+      category[item.category] = item.category
+      total_amount += Number(item?.price?.substring(1) * item.quantity)
+    })
+
+    let obj = { ...overAllStoreDate }
+    obj["no_of_category"] = Object.keys(category)?.length
+    obj["total_value"] = total_amount
+    obj['total_products'] = data.length
+    obj['out_of_stock'] = disabledProducts.length
+
+    setOverAllStoreData({ ...obj })
+
+  }
+
+  useEffect(() => {
+    handleOverAllStoreData()
+  }, [disabledProducts, data])
+
+
+  useEffect(() => {
+    console.log('inside')
+    // getData()
+    setDisabledProducts([])
+    handleOverAllStoreData()
+  }, [])
+
+
+
+  return (
+    <>
+      <div style={{ background: "black", height: "100Vh", width: "100vw" }}>
+        <div style={{ height: '50px', display: "flex", alignItems: "center", gap: 10, justifyContent: 'flex-end' }}>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <div>admin</div>
+            <Switch {...label} onChange={handleChange} size="small" color="warning" />
+            <div>user</div>
+          </div>
+          <div style={{ marginRight: 10 }}>
+            <i class="fa fa-sign-out" aria-hidden="true"></i>
+          </div>
+        </div>
+        <div style={{ marginLeft: 10, marginRight: 10 }}>
+          <div style={{ height: '160px', padding: 0 }}>
+            <div style={{ padding: 0, height: '20%' }}><h2 style={{ margin: 0, marginBottom: 10 }}>Inventory stats</h2></div>
+            <div style={{ height: 10 }}></div>
+            <div style={{ height: '80%', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+              <div style={{ width: '24%', height: "90%", borderRadius: '20px', background: "#2c5421", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
+
+                <div style={{ height: '30%', width: '90%', display: "flex" }}>
+                  <div style={{ width: '15%', height: '100%', display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <ShoppingCartIcon />
+                  </div>
+                  <div style={{ width: '85%', height: '100%', display: "flex", alignItems: "center", fontWeight: "500" }}>Total Product</div>
+                </div>
+
+                <div style={{ height: '50%', width: '90%', display: 'flex' }}>
+                  <div style={{ width: '15%', height: '100%' }}></div>
+                  <div style={{ width: '85%', height: '100%', display: "flex", alignItems: "center", fontSize: "40px", fontWeight: "bold" }}>{data?.length}</div>
+                </div>
+
+              </div>
+
+              <div style={{ width: '24%', height: "90%", borderRadius: '20px', background: "#2c5421", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
+
+                <div style={{ height: '30%', width: '90%', display: "flex" }}>
+                  <div style={{ width: '15%', height: '100%', display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <CurrencyExchangeIcon />
+                  </div>
+                  <div style={{ width: '85%', height: '100%', display: "flex", alignItems: "center", fontWeight: "500" }}>Total Store Value</div>
+                </div>
+
+                <div style={{ height: '50%', width: '90%', display: 'flex' }}>
+                  <div style={{ width: '15%', height: '100%' }}></div>
+                  <div style={{ width: '85%', height: '100%', display: "flex", alignItems: "center", fontSize: "40px", fontWeight: "bold" }}>{overAllStoreDate.total_value}</div>
+                </div>
+
+              </div>
+
+              <div style={{ width: '24%', height: "90%", borderRadius: '20px', background: "#2c5421", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
+
+                <div style={{ height: '30%', width: '90%', display: "flex" }}>
+                  <div style={{ width: '15%', height: '100%', display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <RemoveShoppingCartIcon />
+                  </div>
+                  <div style={{ width: '85%', height: '100%', display: "flex", alignItems: "center", fontWeight: "500" }}>Out of stock</div>
+                </div>
+
+                <div style={{ height: '50%', width: '90%', display: 'flex' }}>
+                  <div style={{ width: '15%', height: '100%' }}></div>
+                  <div style={{ width: '85%', height: '100%', display: "flex", alignItems: "center", fontSize: "40px", fontWeight: "bold" }}>{overAllStoreDate.out_of_stock}</div>
+                </div>
+
+              </div>
+
+              <div style={{ width: '24%', height: "90%", borderRadius: '20px', background: "#2c5421", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column" }}>
+
+                <div style={{ height: '30%', width: '90%', display: "flex" }}>
+                  <div style={{ width: '15%', height: '100%', display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <CategoryIcon />
+                  </div>
+                  <div style={{ width: '85%', height: '100%', display: "flex", alignItems: "center", fontWeight: "500" }}>No of Category</div>
+                </div>
+
+                <div style={{ height: '50%', width: '90%', display: 'flex' }}>
+                  <div style={{ width: '15%', height: '100%' }}></div>
+                  <div style={{ width: '85%', height: '100%', display: "flex", alignItems: "center", fontSize: "40px", fontWeight: "bold" }}>{overAllStoreDate.no_of_category}</div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          {data.length > 0 && <div style={{ border: "1px solid #636363", minHeight: '100px', borderRadius: 20, marginTop: 30, background: 'rgb(100, 102, 100,0.3)' }}>
+
+            <div style={{ display: "flex", flexDirection: "row", borderBottom: "1px solid #636363", height: 50 }}>
+              <div style={{ width: "25%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                <span style={{ padding: '3px 10px', borderRadius: 10, background: 'black', color: "#547d38" }}>Name</span>
+              </div>
+              <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                <span style={{ padding: '3px 10px', borderRadius: 10, background: 'black', color: "#547d38" }}>Category</span>
+              </div>
+              <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                <span style={{ padding: '3px 10px', borderRadius: 10, background: 'black', color: "#547d38" }}>Price</span>
+              </div>
+              <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                <span style={{ padding: '3px 10px', borderRadius: 10, background: 'black', color: "#547d38" }}>Quantity</span>
+              </div>
+              <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                <span style={{ padding: '3px 10px', borderRadius: 10, background: 'black', color: "#547d38" }}>Value</span>
+              </div>
+              <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                <span style={{ padding: '3px 10px', borderRadius: 10, background: 'black', color: "#547d38" }}>Action</span>
+              </div>
+            </div>
+
+            {
+              data.map((item, index) => {
+                return <div style={{ display: "flex", flexDirection: "row", borderBottom: data.length - 1 == index ? "" : "1px solid #636363", height: 60 }}>
+                  <div style={{ width: "25%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                    <span style={{ padding: '3px 10px' }}>{item.name}</span>
+                  </div>
+                  <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                    <span style={{ padding: '3px 10px' }}>{item.category}</span>
+                  </div>
+                  <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                    <span style={{ padding: '3px 10px' }}>{item.price}</span>
+                  </div>
+                  <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                    <span style={{ padding: '3px 10px' }}>{item.quantity}</span>
+                  </div>
+                  <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                    <span style={{ padding: '3px 10px' }}>{"$" + item?.price?.substring(1) * item.quantity}</span>
+                  </div>
+                  <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                    <span style={{ padding: '3px 10px', display: 'flex' }}>
+                      <div style={{ margin: "0 5px" }}>
+                        {checked ? <CreateIcon sx={{ color: 'gray', fontSize: 15 }} /> :
+                          <CreateIcon sx={{ color: 'green', fontSize: 15 }} onClick={() => {
+                            console.log('clicked')
+                            setEditableProduct(item)
+                            setOpenModal(true)
+                          }} />}
+                      </div>
+                      <div style={{ margin: "0 5px" }}>
+                        {
+                          checked ? <VisibilityIcon sx={{ color: 'gray', fontSize: 15 }} /> :
+                            <VisibilityIcon sx={{ color: '#cdaed6', fontSize: 15 }} onClick={() => {
+                              console.log('clicked')
+                              handleDisable(item, index)
+                            }} />
+                        }
+                      </div>
+                      <div style={{ margin: "0 5px" }}>
+
+                        {checked ? <DeleteIcon sx={{ color: "gray", fontSize: 15 }} /> :
+                          <DeleteIcon sx={{ color: 'red', fontSize: 15 }} onClick={() => {
+                            handleDelete(item, index, true)
+                          }} />}
+
+                      </div>
+                    </span>
+                  </div>
+                </div>
+              })
+            }
+          </div>}
+
+
+          {disabledProducts.length > 0 && <h2>Disabled Products</h2>}
+          {disabledProducts.length > 0 && <div style={{ border: "1px solid #636363", minHeight: '100px', borderRadius: 20, marginTop: 30, background: 'rgb(100, 102, 100,0.3)' }}>
+
+            <div style={{ display: "flex", flexDirection: "row", borderBottom: "1px solid #636363", height: 50 }}>
+              <div style={{ width: "25%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                <span style={{ padding: '3px 10px', borderRadius: 10, background: 'black', color: "#547d38" }}>Name</span>
+              </div>
+              <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                <span style={{ padding: '3px 10px', borderRadius: 10, background: 'black', color: "#547d38" }}>Category</span>
+              </div>
+              <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                <span style={{ padding: '3px 10px', borderRadius: 10, background: 'black', color: "#547d38" }}>Price</span>
+              </div>
+              <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                <span style={{ padding: '3px 10px', borderRadius: 10, background: 'black', color: "#547d38" }}>Quantity</span>
+              </div>
+              <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                <span style={{ padding: '3px 10px', borderRadius: 10, background: 'black', color: "#547d38" }}>Value</span>
+              </div>
+              <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                <span style={{ padding: '3px 10px', borderRadius: 10, background: 'black', color: "#547d38" }}>Action</span>
+              </div>
+            </div>
+
+            {
+              disabledProducts.map((item, index) => {
+                return <div style={{ display: "flex", flexDirection: "row", borderBottom: disabledProducts.length - 1 == index ? "" : "1px solid #636363", height: 60 }}>
+                  <div style={{ width: "25%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                    <span style={{ padding: '3px 10px' }}>{item.name}</span>
+                  </div>
+                  <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                    <span style={{ padding: '3px 10px' }}>{item.category}</span>
+                  </div>
+                  <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                    <span style={{ padding: '3px 10px' }}>{item.price}</span>
+                  </div>
+                  <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                    <span style={{ padding: '3px 10px' }}>{item.quantity}</span>
+                  </div>
+                  <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                    <span style={{ padding: '3px 10px' }}>{"$" + item?.price?.substring(1) * item.quantity}</span>
+                  </div>
+                  <div style={{ width: "15%", display: "flex", alignItems: "center", paddingLeft: 10 }}>
+                    <span style={{ padding: '3px 10px', display: 'flex' }}>
+                      <div style={{ margin: "0 5px" }}>
+                        <CreateIcon sx={{ color: 'gray', fontSize: 15 }} />
+                      </div>
+                      <div style={{ margin: "0 5px" }}>
+                        {
+                          checked ? <VisibilityOffIcon sx={{ color: 'gray', fontSize: 15 }} /> :
+                            <VisibilityOffIcon sx={{ color: '#cdaed6', fontSize: 15 }} onClick={() => {
+                              console.log('clicked')
+                              handleEnable(item, index)
+                            }} />
+                        }
+                      </div>
+                      <div style={{ margin: "0 5px" }}>
+
+                        {checked ? <DeleteIcon sx={{ color: "gray", fontSize: 15 }} /> :
+                          <DeleteIcon sx={{ color: 'red', fontSize: 15 }} onClick={() => {
+                            handleDelete(item, index, false)
+                          }} />}
+
+                      </div>
+                    </span>
+                  </div>
+                </div>
+              })
+            }
+          </div>}
+
+        </div>
+        <BasicModal modalState={openModal} handleModalState={handleModalState} editableProduct={editableProduct} handleEdit={handleEdit} />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/modal.jsx
@@ -0,0 +1,133 @@
+import * as React from 'react';
+import { useEffect, useState } from 'react'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import CloseIcon from '@mui/icons-material/Close';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  height: 200,
+  // bgcolor: 'background.paper',
+  bgcolor: "#1c1b1b",
+  // border: '2px solid #000',
+  borderRadius: 5,
+  boxShadow: 24,
+  p: 3,
+};
+
+export default function BasicModal({ modalState, handleModalState, editableProduct ,handleEdit}) {
+  const [open, setOpen] = useState(false);
+  const [currentProduct, setCurrentProduct] = useState({})
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false)
+    handleModalState(false)
+  };
+
+
+  const handleCurrentProduct = (key, value) => {
+    let obj = { ...currentProduct }
+    obj[key] = value
+    setCurrentProduct({ ...obj })
+  }
+
+  useEffect(() => {
+    setOpen(modalState)
+  }, [modalState])
+
+  useEffect(() => {
+    let obj = {...editableProduct}
+    obj.price = editableProduct?.price?.substring(1)
+    setCurrentProduct({ ...obj })
+  }, [editableProduct])
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        onClose={(event, reason) => {
+          if (reason !== 'backdropClick' && reason !== 'escapeKeyDown') {
+            handleClose()
+          }
+        }}
+      >
+        <Box sx={style}>
+          <div style={{ height: '100%', background: "#1c1b1b", display: "flex", flexDirection: 'column' }}>
+            <div style={{ height: '15%', width: '100%', display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+              <p style={{ fontSize: 20 }}>Edit Product</p>
+
+              <CloseIcon sx={{ color: "#547d38", borderRadius: 1, background: 'rgb(100, 102, 100,0.3)' }} onClick={handleClose} />
+
+            </div>
+
+            <div style={{ height: '10%', width: '100%', display: "flex" }}>
+              <div style={{ fontSize: 10 }}>{currentProduct.name}</div>
+            </div>
+
+            <div style={{ height: '60%', width: '100%', display: "flex", flexDirection: "column", marginTop: 5 }}>
+              <div style={{ height: '50%', display: "flex", justifyContent: "space-between" }}>
+                <div style={{ width: '48%' }}>
+                  <label style={{ fontSize: 12 }}>Category</label>
+                  <input type='text' value={currentProduct?.category} onChange={(e) => { handleCurrentProduct("category", e.target.value) }} style={{ padding: 5, width: '95%', borderRadius: 5, border: 0, fontSize: 12, color: 'gray' }} />
+                </div>
+                <div style={{ width: '48%' }}>
+                  <label style={{ fontSize: 12 }}>Price</label>
+                  <input type='text' value={currentProduct?.price} onChange={(e) => {
+                    if(!isNaN(Number(e.target.value))){
+                      handleCurrentProduct("price", e.target.value)
+                    }else{
+                      return
+                    }
+                      
+                  }} style={{ padding: 5, width: '95%', borderRadius: 5, border: 0, fontSize: 12, color: 'gray' }} />
+                </div>
+              </div>
+
+              <div style={{ height: '50%', display: "flex", justifyContent: "space-between" }}>
+                <div style={{ width: '48%' }}>
+                  <label style={{ fontSize: 12 }}>Quantity</label>
+                  <input type='text' value={currentProduct.quantity} onChange={(e) => {
+
+                    handleCurrentProduct("quantity", e.target.value)
+
+
+                  }} style={{ padding: 5, width: '95%', borderRadius: 5, border: 0, fontSize: 12, color: 'gray' }} />
+                </div>
+                <div style={{ width: '48%' }}>
+                  <label style={{ fontSize: 12 }}>Value</label>
+                  <input type='text' value={currentProduct.value} onChange={(e) => { handleCurrentProduct("value", e.target.value) }} style={{ padding: 5, width: '95%', borderRadius: 5, border: 0, fontSize: 12, color: 'gray' }} />
+                </div>
+              </div>
+
+            </div>
+
+            <div style={{ height: '15%', width: '100%', display: "flex", justifyContent: "end", marginTop: 5 }}>
+              <div style={{ color: '#547d38' }} onClick={()=>{
+                setCurrentProduct({...editableProduct})
+                handleClose()
+              }}>Cancle</div>
+              <button style={{ display: "flex", alignItems: "center", justifyContent: "center", marginRight: 10, marginLeft: 20, background: 'rgb(100, 102, 100,0.3)', color: "gray" }} onClick={()=>{
+                let obj = {...currentProduct}
+                obj['price'] =  '$'+ currentProduct['price']
+                handleEdit(editableProduct,obj)
+                handleClose()
+                }}>Save</button>
+            </div>
+
+
+          </div>
+
+        </Box>
+      </Modal>
+    </div>
+  );
+}
